57dae96084d619706e13daaf2a3fcc68936bf034
Kristen Lee <leekristen090@gmail.com>
2024-06-12 13:11:20 -0400

    Add files via upload

---
+++ A4/LinkedList.cpp
@@ -0,0 +1,66 @@
+// Assignment 4
+// CS 341
+
+// Honor Pledge: 
+//
+// I pledge that I have neither given nor
+// receieved help on this assignment. 
+//
+// kmlee1
+
+// LinkedList.cpp
+#include "LinkedList.h"
+
+LinkedList::LinkedList(): head_(nullptr), tail_(nullptr), length_(0) {}
+
+LinkedList::~LinkedList() 
+{
+    if(head_!=nullptr)
+    { 
+        delete head_; 
+    }
+}
+
+bool LinkedList::isEmpty()
+{ return head_==nullptr; }
+
+int LinkedList::getLength()
+{ return length_; }
+
+void LinkedList::insert(int element)
+{  
+    //std::cout<< "Inserting "<< element<< "..." <<std::endl;
+    LinkedNode*newNode= new LinkedNode(element);
+    if(isEmpty())
+    { 
+        //std::cout<< "List was empty and now we are inserting."<<std::endl; // for debugging 
+        head_=newNode;
+        tail_=newNode;
+    }
+    else
+    {
+        //std::cout<< "List is not empty so we are updating the tail."<<std::endl; // for debugging
+        tail_->setNextLinkedNode(newNode);
+        newNode->setPrevLinkedNode(tail_);
+        tail_=newNode;
+    }
+    length_++;
+    //std::cout<< "Head: "<< head_->getData()<< " Tail: "<< tail_->getData()<< std::endl;// for debugging
+    //std::cout<<"Length: "<<getLength()<<std::endl;
+}
+
+void LinkedList::printList()
+{ 
+    std::cout<< "\nPrinting list: "<<std::endl;
+    LinkedNode* current=head_;
+
+    while(current!=nullptr)
+    {
+        std::cout<< current->getData();
+        if(current->hasNextLinkedNode()==false)
+        { std::cout<<" --> "; }
+        else 
+        { std::cout<< " (End of list)"<< std::endl; }
+        current=current->getNextLinkedNode();
+    }
+}
---
+++ A4/LinkedNode.h
@@ -0,0 +1,35 @@
+// Assignment 4
+// CS 341
+
+// Honor Pledge: 
+//
+// I pledge that I have neither given nor
+// receieved help on this assignment. 
+//
+// kmlee1
+
+// LinkedNode.h
+#ifndef LINKEDNODE_H
+#define LINKEDNODE_H
+
+#include "node.h"
+
+class LinkedNode: public Node
+{
+    public:
+        LinkedNode() {}
+        LinkedNode(int data);
+        LinkedNode(int data, LinkedNode*nextLinkedNode, LinkedNode*prevLinkedNode);
+        virtual ~LinkedNode();
+        LinkedNode*getNextLinkedNode();
+        void setNextLinkedNode(LinkedNode*nextLinkedNode);
+        LinkedNode*getPrevLinkedNode();
+        void setPrevLinkedNode(LinkedNode*prevLinkedNode);
+        bool hasNextLinkedNode();
+        bool hasPrevLinkedNode();
+    private:
+        LinkedNode*nextLinkedNode_;
+        LinkedNode*prevLinkedNode_;
+};
+
+#endif
