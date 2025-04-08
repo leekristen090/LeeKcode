48133de95f656e9772480809dbfdfd21882f510a
Kristen Lee <leekristen090@gmail.com>
2025-04-08 15:52:16 -0400

    Add files via upload

--- NEU_Fall_2024/CS5010/Assignments/Group/A5/src/view/Main.java
+++ NEU_Fall_2024/CS5010/Assignments/Group/A5/src/view/Main.java
@@ -1,1 +1,35 @@
-//
+package view;
+
+import java.io.IOException;
+
+import controller.ImageController;
+import controller.Interactive;
+import controller.ScriptExecutor;
+
+/**
+ * This is our main class in which we call our interactive class to allow for interactive command
+ * line input from the user.
+ */
+public class Main {
+
+  /**
+   * This is our main class so we can do interactive user input.
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    ImageController controller = new ImageController();
+    ScriptExecutor scriptExecutor = new ScriptExecutor(controller);
+    Interactive interactive = new Interactive(controller, scriptExecutor);
+
+    try {
+      scriptExecutor.executeScript("../res/Script.txt");
+      //interactive.executeInteractive();
+    } catch (IOException e) {
+      System.err.println("Error executing script: " + e.getMessage());
+    }
+  }
+
+
+}
+
